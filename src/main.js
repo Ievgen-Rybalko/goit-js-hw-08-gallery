4b3c6608c714986c8ef6f16fc98b1ffec2480126
Ievgen-Rybalko <jackrjack1@gmail.com>
2021-04-25 18:32:02 +0300

    upd: task done with add-ons

---
+++ src/main.js
@@ -0,0 +1,99 @@
+import galleryItems from "./gallery-items.js";
+
+const galleryItemsLength = galleryItems.length;
+
+//create a gallery markup basing on imported file:
+function createGalleryMarkup ({ preview, original, description }) {
+    return `
+    <li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;};
+
+const allGalleryLiMarkup = galleryItems.map(createGalleryMarkup).join('');
+
+const galleryUlRef = document.querySelector('ul.js-gallery');
+galleryUlRef.insertAdjacentHTML('afterbegin', allGalleryLiMarkup);
+
+// on image click, put attributes to show image in modal window
+let currentPosInGallery;
+let lastImgOpen;
+
+const lightBoxRef = document.querySelector('.js-lightbox');
+const imgInModalShow = document.querySelector('.lightbox__image');
+
+function onPictureClick(e) {
+    e.preventDefault();
+    const clickedImageAddress = e.target;
+               
+    imgInModalShow.src = clickedImageAddress.getAttribute('data-source');
+    imgInModalShow.alt = clickedImageAddress.alt;
+
+    lightBoxRef.classList.toggle("is-open");
+
+    //find current image clicked:
+    const galleryItemsOriginals = galleryItems.map(({ original }) => original);
+    currentPosInGallery = galleryItemsOriginals.indexOf(imgInModalShow.src);
+}
+
+galleryUlRef.addEventListener('click', onPictureClick);
+
+//on close modal button click
+const closeBtnRef = document.querySelector('button[data-action="close-lightbox"]');
+
+function onCloseBtnClick() {
+    
+    lightBoxRef.classList.toggle("is-open");
+    
+    imgInModalShow.src = '';
+    imgInModalShow.alt = '';
+    //closeBtnRef.removeEventListener('click', onCloseBtnClick);   
+}
+
+closeBtnRef.addEventListener('click', onCloseBtnClick);
+
+//modal window close with "ESC", arrows <-  -> handling
+function onKeyDown(e) {
+        
+    if (e.key == 'Escape') {
+        lightBoxRef.classList.toggle("is-open");
+        
+        imgInModalShow.src = '';
+        imgInModalShow.alt = '';
+    }
+    else if (e.key == 'ArrowLeft') {
+        if (currentPosInGallery > 0) {
+            currentPosInGallery -= 1;
+            imgInModalShow.src = galleryItems[currentPosInGallery].original;
+            imgInModalShow.alt = galleryItems[currentPosInGallery].description; 
+        } else {
+            console.log('Это начало. Ходи правый сторона >>>')
+        }
+     }
+    else if (e.key == 'ArrowRight') {
+        if (currentPosInGallery < (galleryItemsLength - 1)) {
+            currentPosInGallery += 1;
+            imgInModalShow.src = galleryItems[currentPosInGallery].original;
+            imgInModalShow.alt = galleryItems[currentPosInGallery].description; 
+        } else {
+            console.log('<<< Это конец. Ходи левый сторона')
+        }
+
+     }
+};
+
+//modal window buttons handling
+window.addEventListener('keydown', onKeyDown);
+
+//modal close on click -> div.lightbox__overlay 
+const lightboxRef = document.querySelector('div.lightbox__overlay');
+lightboxRef.addEventListener('click', onCloseBtnClick);
